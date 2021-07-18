3e57c0c85bbe76cb55ed3bd17d17685978798f6a
Michael Sheng <michaelsheng@Michael.phub.net.cable.rogers.com>
2021-07-17 22:29:54 -0400

    resource page structure created

--- src/Views/Resources.js
+++ src/Views/Resources.js
@@ -1,18 +1,63 @@
 import React from "react";
 import { Row, Col, Container } from "react-bootstrap";
 import NavCard from "../Components/NavCard";
-import Footer from "../Components/Footer";
+import "./Resources.css";
 
-import { Link } from "react-router-dom";
+import { HiOutlineDocumentDuplicate } from "react-icons/hi";
+import { BsFileSpreadsheet } from "react-icons/bs";
+
+
+
 
 const Resources = () => {
   return (
     <div>
       <NavCard />
-      <div className="header">
+      <div className="res-header">
         <h1>Resources</h1>
         <p>Helpful documents and links.</p>
       </div>
+
+      <Container className="res-container">
+        <Row className="res-row">
+          <Col className="res-col">
+            <h3 className="res-sub-header">Documents</h3>
+            <div className="res-box-doc">
+              <a><HiOutlineDocumentDuplicate />Test</a>
+            </div>
+            <div className="res-box-doc">
+              <a><HiOutlineDocumentDuplicate />Test</a>
+            </div>
+            <div className="res-box-doc">
+              <a><HiOutlineDocumentDuplicate />Test</a>
+            </div>
+            <div className="res-box-doc">
+              <a><HiOutlineDocumentDuplicate />Test</a>
+            </div>
+            <div className="res-box-doc">
+              <a><HiOutlineDocumentDuplicate />Test</a>
+            </div>
+            <div className="res-box-doc">
+              <a><HiOutlineDocumentDuplicate />Test</a>
+            </div>
+          </Col>
+          <Col>
+            <h3 className='res-sub-header'>Spreadsheets</h3>
+            <div className="res-box-sheet">
+              <a><BsFileSpreadsheet />Test</a>
+            </div>
+            <div className="res-box-sheet">
+              <a><BsFileSpreadsheet />Test</a>
+            </div>
+            <div className="res-box-sheet">
+              <a><BsFileSpreadsheet />Test</a>
+            </div>
+            <div className="res-box-sheet">
+              <a><BsFileSpreadsheet />Test</a>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
