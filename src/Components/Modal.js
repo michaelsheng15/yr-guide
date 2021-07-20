9160d0782c2891b760725e13e3870e936eecea51
Michael Sheng <michaelsheng@Michael.phub.net.cable.rogers.com>
2021-07-19 22:52:02 -0400

    Created modal

---
+++ src/Components/Modal.js
@@ -0,0 +1,90 @@
+import "./Modal.css";
+import React from "react";
+import { Row, Col, Container } from "react-bootstrap";
+
+import { AiFillGithub } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
+import { AiFillMail } from "react-icons/ai";
+import { BiUser } from "react-icons/bi";
+
+import { IconContext } from "react-icons";
+
+const Modal = ({ handleClose, show, children }) => {
+  const showHideClassName = show ? "modal display-block" : "modal display-none";
+
+  return (
+    <div className={showHideClassName}>
+      <section className="modal-main">
+        <Container className="modal-container">
+          <Row className="modal-row">
+            <Col>
+              <h3>
+                About the Developer! <BiUser />
+              </h3>
+              <p>
+                This website is developed completely from scratch by Michael
+                Sheng in Summer 2021.
+              </p>
+              <p>
+                <b>Tools:</b> React.js | CSS | Bootstrap
+              </p>
+              <p>Have a recommendation? Contact me!</p>
+              <div className="modal-btn-div">
+                <button
+                  className="btn-modal"
+                  type="button"
+                  onClick={handleClose}
+                >
+                  Close
+                </button>
+                <IconContext.Provider
+                  value={{ color: "grey", className: "button-scale" }}
+                >
+                  <button className="button-icon">
+                    <a
+                      target="_blank"
+                      href="https://github.com/michaelsheng15"
+                      rel="noreferrer"
+                    >
+                      <AiFillGithub className="logo" />
+                    </a>
+                  </button>
+                </IconContext.Provider>
+
+                <IconContext.Provider
+                  value={{ color: "grey", className: "button-scale" }}
+                >
+                  <button className="button-icon">
+                    <a
+                      target="_blank"
+                      href="https://www.linkedin.com/in/michaeljsheng/"
+                      rel="noreferrer"
+                    >
+                      <AiFillLinkedin className="logo" />
+                    </a>
+                  </button>
+                </IconContext.Provider>
+
+                <IconContext.Provider
+                  value={{ color: "grey", className: "button-scale" }}
+                >
+                  <button className="button-icon">
+                    <a
+                      target="_blank"
+                      href="mailto:m3sheng@uwaterloo.ca"
+                      rel="noreferrer"
+                    >
+                      <AiFillMail className="logo" />
+                    </a>
+                  </button>
+                </IconContext.Provider>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </div>
+  );
+};
+
+export default Modal;
--- src/Views/Home.js
+++ src/Views/Home.js
@@ -6,40 +6,64 @@ import { Link } from "react-router-dom";
 import "./Home.css";
 import home1 from "../img/home2.png";
 
-const Home = () => {
-  return (
-    <div className="screen">
-      <NavCard />
-      <Container>
-        <Row className="home-row">
-          <Col className="text-col">
-            <h1 className="home-h">Welcome! 👋</h1>
-            <p className="home-p">
-              Redeployed? Co-op student? New employee? This handbook provides a
-              collection of tools and resources to help you adapt quickly to
-              your role as a Support Analyst
-              <span style={{ color: "rgb(4, 0, 255)" }}> @York Region</span>.
-            </p>
+import React, { Component } from "react";
+import Modal from "../Components/Modal";
 
-            <div className="btn-container">
-              <Link to="/guides" style={{ textDecoration: "none" }}>
-                <a className="btn">Get Started!</a>
-              </Link>
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      show: false,
+    };
+    this.showModal = this.showModal.bind(this);
+    this.hideModal = this.hideModal.bind(this);
+  }
 
-              <a className="btn" id="outline">
-                About the Dev!
-              </a>
-            </div>
-          </Col>
-          <Col className="img-col">
-            <img src={home1} className="img" />
-          </Col>
-        </Row>
-      </Container>
+  showModal = () => {
+    this.setState({ show: true });
+  };
 
-      <Footer />
-    </div>
-  );
-};
+  hideModal = () => {
+    this.setState({ show: false });
+  };
 
+  render() {
+    return (
+      <div className="screen">
+        <NavCard />
+        <Container>
+          <Row className="home-row">
+            <Col className="text-col">
+              <h1 className="home-h">Welcome! 👋</h1>
+              <p className="home-p">
+                Redeployed? Co-op student? New employee? This handbook provides
+                a collection of tools and resources to help you adapt quickly to
+                your role as a Support Analyst
+                <span style={{ color: "rgb(4, 0, 255)" }}> @York Region</span>.
+              </p>
+
+              <div className="btn-container">
+                <Link to="/guides" style={{ textDecoration: "none" }}>
+                  <a className="btn">Get Started!</a>
+                </Link>
+
+                <Modal show={this.state.show} handleClose={this.hideModal}>
+                  <p>Modal</p>
+                </Modal>
+                <a className="btn" id='outline' onClick={this.showModal}>
+                  About the Dev!
+                </a>
+              </div>
+            </Col>
+            <Col className="img-col">
+              <img src={home1} className="img" />
+            </Col>
+          </Row>
+        </Container>
+
+        <Footer />
+      </div>
+    );
+  }
+}
 export default Home;
